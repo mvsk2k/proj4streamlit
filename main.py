fb20d5bcb1a17a9419cbbeb066c9bb5de5bf8626
M V SIVAKUMAR <mvsk2k@gmail.com>
2023-03-12 19:56:11 +0530

    base4version

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width = 400)
+    st.image("images/photo.png", width = 500)
 
 with col2:
     st.title("M V SIVAKUMAR")
     content = """
-    This is M V SIVAKUMAR doing some python competency building
+    This is M V SIVAKUMAR doing some python competency building,  Did MTech Embedded Systems
+    and Experienced in Project Management Activity. Doing the Web Application building Python
+    Programming Language, Planning IOT product development.
     """
     #st.write(content)
     st.info(content)
+
+content2 = """
+Below you can find apps that I have built
+"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
+
+
 
