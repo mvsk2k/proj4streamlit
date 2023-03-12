c33bd4f9b9e39e1387a2763cfa3cf788c58cf2b5
M V SIVAKUMAR <mvsk2k@gmail.com>
2023-03-12 20:18:21 +0530

    Added description

--- main.py
+++ main.py
@@ -23,17 +23,25 @@ Below you can find apps that I have built
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+#col3, col4 = st.columns(2)
+col3, emptycol, col4 = st.columns([1.5,0.5,1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        #st.write("[Source code](https://pythonhow.com)")
+        st.write(f"[Source code]({row['url']})")
 
 with col4:
     for index,row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
 
 
 
